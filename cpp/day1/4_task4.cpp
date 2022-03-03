dd977950c03be64f612f06b1908cb63acc21c7e2
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-03 23:40:10 +0400

    Added new file

---
+++ cpp/day1/4_task4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+        int chickens;
+        int cows;
+        int pigs;
+        int result;
+        cout << "chickens = ";
+        cin >> chickens;
+        cout << "cows = ";
+        cin >> cows;
+        cout << "pigs = ";
+        cin >> pigs;
+        result = (chickens * 2) + (cows * 4) + (pigs * 4);
+        cout << "result = " << result << endl;
+return 0;
+}
+

6e2cf37a5eb7967ffb54371fea3a08b57f6f90ba
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-05 20:44:25 +0400

    Added a new file

---
+++ cpp/day2/3_alphabet.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	char alphabet;
+	cout << "Input an alphabet: ";
+	cin >> alphabet;
+
+	switch (alphabet)
+	{
+		case 'a':
+			cout << "'a' is a vowel" << endl;
+			break;
+		case 'e':
+			cout << "'e' is a vowel" << endl;
+			break;
+		case 'i':
+			cout << "'i' is a vowel" << endl;
+			break;
+		case 'o':
+			cout << "'o' is a vowel" << endl;
+			break;
+		case 'u':
+			cout << "'u' is a vowel" << endl;
+			break;
+		case 'b':
+			cout << "'b' is a consonant" << endl;
+			break;
+		case 'c':
+			cout << "'c' is a consonant" << endl;
+			break;
+		case 'd':
+			cout << "'d' is a consonant" << endl;
+			break;
+		case 'f':
+			cout << "'f' is a consonant" << endl;
+			break;
+		case 'g':
+			cout << "'g' is a consonant" << endl;
+			break;
+		case 'h':
+			cout << "'h' is a consonant" << endl;
+			break;	
+		case 'j':
+			cout << "'j' is a consonant" << endl;
+			break;
+		case 'k':
+			cout << "'k' is a consonant" << endl;
+			break;
+		case 'l':
+			cout << "'l' is a consonant" << endl;
+			break;
+		case 'm':
+			cout << "'m' is a consonant" << endl;
+			break;
+		case 'n':
+			cout << "'n' is a consonant" << endl;
+			break;
+		case 'p':
+			cout << "'p' is a consonant" << endl;
+			break;
+		case 'q':
+			cout << "'q' is a consonant" << endl;
+			break;
+		case 'r':
+			cout << "'r' is a consonant" << endl;
+			break;
+		case 's':
+			cout << "'s' is a consonant" << endl;
+			break;
+		case 't':
+			cout << "'t' is a consonant" << endl;
+			break;
+		case 'v':
+			cout << "'v' is a consonant" << endl;
+			break;
+		case 'w':
+			cout << "'w' is a consonant" << endl;
+			break;
+		case 'x':
+			cout << "'x' is a consonant" << endl;
+			break;
+		case 'y':
+			cout << "'y' is a consonant" << endl;
+			break;
+		case 'z':
+			cout << "'z' is a consonant" << endl;
+			break;
+
+		default:
+			cout << "Error: input an alphabet" << endl;
+	}
+	return 0;
+   }                          
+

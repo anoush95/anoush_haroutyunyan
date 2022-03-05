8b43d69ec29c7b84446ecb980874e2380f20b676
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-05 19:11:23 +0400

    Made some changes

---
+++ cpp/day1/6_factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+        int number;
+        int factorial = 1;
+        int i = 1;
+
+        cout << "number: ";
+        cin >> number;
+
+        while (i <= number)
+        {
+                factorial = factorial * i;
+                i++;
+        }
+
+        cout << "The factorial of " << number << " is " << factorial << endl;
+
+return 0;
+}
+

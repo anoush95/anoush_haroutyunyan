1875f4b1c3140dce8519f6143cfbb8bf3eab6152
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-08 18:11:57 +0400

    Added a new file

---
+++ cpp/day3/1_game.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void result(int x, int y)
+{
+	do
+        {
+                if (y < x)
+                {
+                        cout << "Enter a larger number:\n";
+                        cin >> y;
+                }
+                else if (y > x)
+                {
+                        cout << "Enter a smaller number:\n";
+                        cin >> y;
+                }
+                else
+                {
+                        break;
+                }
+
+
+        }
+
+        while (y != x);
+        cout << "You guessed the number!\n";
+
+}
+int main() 
+{
+
+        int x;
+        cout << "Enter a number:\n";
+        cin >> x;
+
+        int y;
+        cout << "Guess the number:\n";
+        cin >> y;
+	
+	result(x, y);
+
+
+        return 0;
+}
+

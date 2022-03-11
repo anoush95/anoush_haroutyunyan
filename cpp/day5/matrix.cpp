0fd76f9439e730690d1ae4038b32cac228b1a449
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-11 23:27:16 +0400

    Made some changes

--- cpp/day5/matrix.cpp
+++ cpp/day5/matrix.cpp
@@ -15,10 +15,8 @@ int main()
                 {
                         matrix[i][j] = (rand()%10);
                         {
-
                                 cout << matrix[i][j]<<" ";
                         }
-
                 }
                 cout << endl;
 	}
@@ -29,18 +27,15 @@ int main()
 			if(i != j && i < j)
 			{			
 				lower_left_triangle += matrix[i][j];
-			
 			}
 			if( i != j && i > j)
 			{
 				upper_right_triangle += matrix[i][j];
-				
 			}
 			if((i + j < n - 1) && (i<=n - 1 || j <= n - 1))
 			{
 				upper_left_triangle += matrix[i][j];
 			}
-
 			if((i + j >= n) && (i < n || j == n - 1 || j < n ))
 			{
 				lower_right_triangle += matrix[i][j];
@@ -67,8 +62,9 @@ int main()
 		}
 	}
 	cout << endl << "Lower left triangle: " << lower_left_triangle << endl << "Upper right triangle: " <<  upper_right_triangle << endl 
-	<< "Upper left triangle: " << upper_left_triangle << endl << "Lower right triangle: " << lower_right_triangle << endl << "Upper triangle: " << upper_triangle << endl 
-	<< "Right triangle: " << right_triangle << endl << "Lower triangle: " << lower_triangle << endl << "Left triangle: " <<left_triangle << endl;
+	<< "Upper left triangle: " << upper_left_triangle << endl << "Lower right triangle: " << lower_right_triangle << endl 
+	<< "Upper triangle: " << upper_triangle << endl << "Right triangle: " << right_triangle << endl << "Lower triangle: " 
+	<< lower_triangle << endl << "Left triangle: " <<left_triangle << endl;
 	       
 return 0;
 }

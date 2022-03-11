35dd4d79457667efff230e2c26b15efc77c03c94
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-11 14:04:15 +0400

    Made changes

--- cpp/day5/matrix.cpp
+++ cpp/day5/matrix.cpp
@@ -21,28 +21,7 @@ int main()
                 }
                 cout << endl;
 
-        }
-		cout << endl;
-
-		sum1 = matrix[1][0] + matrix[2][0] + matrix[2][1] + matrix[3][0];
-		
-		sum2 = matrix[0][1] + matrix[0][2] + matrix[0][3] + matrix[1][2];
-	
-		sum3 = matrix[1][4] + matrix[2][3] + matrix[2][4] + matrix[3][4];
-            
-		sum4 = matrix[3][2] + matrix[4][1] + matrix[4][2] + matrix[4][3];
-                  
-                sum5 = sum1 + sum2;
-	    		
-		sum6 = sum2 + sum3;
-
-		sum7 = sum3 + sum4;
-
-		sum8 = sum4 + sum1;
-                
-		
-		cout << sum1 << endl << sum2 << endl << sum3 << endl << sum4 << endl << sum5 << endl << sum6 << endl << sum7 << endl << sum8 << endl;
-                        
+	}        
 return 0;
 }
 

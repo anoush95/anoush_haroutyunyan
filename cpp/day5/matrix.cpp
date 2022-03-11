244f89df2417d380b68ec8614d7bd41b35ab425c
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-11 23:23:39 +0400

    Made some changes

--- cpp/day5/matrix.cpp
+++ cpp/day5/matrix.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main()
 {
-        int lower_left_triangle = 0, upper_right_triangle = 0, upper_left_triangle = 0, lower_right_triangle = 0, upper_triangle = 0, right_triangle = 0, lower_triangle = 0, left_triangle = 0, i, j;
+        int lower_left_triangle = 0, upper_right_triangle = 0, upper_left_triangle = 0, lower_right_triangle = 0, upper_triangle = 0, right_triangle = 0, 
+	lower_triangle = 0, left_triangle = 0, i, j;
         const int n = 5;
         int matrix[n][n];
 
@@ -30,14 +31,11 @@ int main()
 				lower_left_triangle += matrix[i][j];
 			
 			}
-			
-			
 			if( i != j && i > j)
 			{
 				upper_right_triangle += matrix[i][j];
 				
 			}
-
 			if((i + j < n - 1) && (i<=n - 1 || j <= n - 1))
 			{
 				upper_left_triangle += matrix[i][j];
@@ -45,7 +43,6 @@ int main()
 
 			if((i + j >= n) && (i < n || j == n - 1 || j < n ))
 			{
-				
 				lower_right_triangle += matrix[i][j];
 			}
 			if(i != j && i + j != n - 1)
@@ -69,9 +66,9 @@ int main()
 			}
 		}
 	}
-	cout << endl << "Lower left triangle: " << lower_left_triangle << endl << "Upper right triangle: " <<  upper_right_triangle << endl << "Upper left triangle: " << upper_left_triangle << endl 
-	<< "Lower right triangle: " << lower_right_triangle << endl << "Upper triangle: " << upper_triangle << endl << "Right triangle: " << right_triangle << endl << "Lower triangle: " 
-	<< lower_triangle << endl << "Left triangle: " <<left_triangle << endl;
+	cout << endl << "Lower left triangle: " << lower_left_triangle << endl << "Upper right triangle: " <<  upper_right_triangle << endl 
+	<< "Upper left triangle: " << upper_left_triangle << endl << "Lower right triangle: " << lower_right_triangle << endl << "Upper triangle: " << upper_triangle << endl 
+	<< "Right triangle: " << right_triangle << endl << "Lower triangle: " << lower_triangle << endl << "Left triangle: " <<left_triangle << endl;
 	       
 return 0;
 }

f6b2b82b34f810ed46b64f9e9367afb8eefd25f4
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-12 02:00:09 +0400

    Made some changes

--- cpp/day3/2_multiplication.cpp
+++ cpp/day3/2_multiplication.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
 using namespace std;
 
-void mult(int i, int number)
+void mult(int number)
 {		
+	int i = 1;
 	while (i <= 10)
 	{
 		cout << number << "x" << i << "=" << number * i << endl;
 		i++;
-	
 	}
-	
 }
-
 int main () 
 {
-	int i = 1;
         int number;
-
         cout << "number: ";
         cin >> number;
-	
-        mult(i, number);
+        mult(number);
 
 return 0;
 }

ceec4fdcdb09f5ac5c42c87af808bd350e7b4597
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-16 02:32:50 +0400

    made some corrections to the function

--- cpp/day7/3_sum.cpp
+++ cpp/day7/3_sum.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 using namespace std;
+void sum_of_array_elements (int *pointer, int *number);
 
 int main ()
 {
-	int n = 7;
-        int arr[n];
-        int sum = 0, i;
-        int *pointer;
-        cout << "Enter 7 numbers: \n";
-        for (i = 0; i < n; i++)
-        {
-        	cin >> arr[i];
-        }
-        pointer = arr;
-        i = 0;
-        while(i < n)
-        {
-                sum = sum + *pointer;
-                pointer++;
-                i ++;
-        }
-        cout << "The sum is " << sum << endl;
+	int num;
+	cout << "Input the number of elements: ";
+	cin >> num;
+	int *array = new int[num];
+	cout << "Input the elements:\n";
+	int *pointer = array;
+	while (pointer < array + num)
+	{
+	cin >> *pointer++;
+	}
+	cout << endl;
+	pointer = array;
+	while (pointer < array + num)
+  	{     
+		cout << *pointer++ << " ";
+	}
+	sum_of_array_elements(array, &num);
 return 0;
 }
 
+void sum_of_array_elements (int *pointer, int *number)
+{	
+	int sum = 0;
+	int *ptr = pointer;
+	while (ptr < pointer + *number)
+    	{	
+      		sum = sum + *ptr;
+	 	ptr++;	
+        }
+        cout << endl << "The sum of the elements in the array is " << sum << endl;
 
+}

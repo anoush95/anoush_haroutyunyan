28720670c7500c2d654f4fa38121769adc1d59fb
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-16 02:30:35 +0400

    Made some corrections

--- cpp/day7/1_functions.cpp
+++ cpp/day7/1_functions.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-void sum1(int *a, int *b, int *sum);
-void sum2(int &a, int &b, int &sum);
-void sum3(int a, int b, int &sum);
-void sum4(int a, int *b, int &sum);
-void sum5(int *a, int *b, int &sum);
+void sum1 (int *a, int *b, int *sum);
+void sum2 (int &a, int &b, int &sum);
+void sum3 (int a, int b, int &sum);
+void sum4 (int &a, int *b, int &sum);
+void sum5 (int *a, int *b, int &sum);
 
 int main ()
 {
@@ -17,25 +17,39 @@ int main ()
 	cout << "Input the second number: ";
 	cin >> y;
 
-	sum1(&x, &y, &s);
-	sum2(x, y, s);
+	sum1 (&x, &y, &s);
+	sum2 (x, y, s);
+	sum3 (x, y, s);
+	sum4 (x, &y, s);
+	sum5 (&x, &y, s);
 
 return 0;
 }
-void sum1(int *a, int *b, int *sum)
+void sum1 (int *a, int *b, int *sum)
 {
 	*sum = *a + *b;
 	cout << "sum1 = " << *sum << endl;
 }
 
-void sum2(int &a, int &b, int &sum)
+void sum2 (int &a, int &b, int &sum)
 {
-	int *a_address, *b_address, *sum_address;
-	a_address = &a;
-      	b_address = &b;
-	sum_address = &sum;
-	sum = *a_address + *b_address; 	
+	sum = a + b;
 	cout << "sum2 = " << sum << endl;
+}
+void sum3 (int a, int b, int &sum)
+{
+	sum = a + b;
+	cout << "sum3 = " << sum << endl;
+}
+void sum4 (int &a, int *b, int &sum)
+{
+	sum = a + *b;
+	cout << "sum4 = " << sum << endl;
+}
+void sum5 (int *a, int *b, int &sum)
+{
+	sum = *a + *b;
+	cout << "sum5 = " << sum << endl;
 }
 
 

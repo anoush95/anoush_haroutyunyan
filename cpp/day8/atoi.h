d3eed3fc30c76bb63d365fbc1ed32bf7f38c3c7b
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-17 01:20:45 +0400

    Separated the implementation part from the header file

--- cpp/day8/atoi.h
+++ cpp/day8/atoi.h
@@ -1,19 +1,11 @@
-#include <iostream>
-using namespace std;
+#ifndef ATOI_H
+#define ATOI_H
 
-int myAtoi (char* string) 
-{
-   int result = 0;
-   for (int i = 0; string[i] != '\0'; i++) 
-   {
-	if(string[i] >= 48 && string[i] <= 57)
-	{
-      		result = result * 10 + string[i] - '0';
-	}
-	else
-	{
-		result = 0;
-	}
-   } 
-   return result;
-}
+
+
+int myAtoi (char* string);
+
+#endif
+
+
+

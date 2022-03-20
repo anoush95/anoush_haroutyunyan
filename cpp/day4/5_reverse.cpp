f56ad28c11702ffdc2ae3094315943dbbf854b6b
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-21 01:27:50 +0400

    Added the random function

--- cpp/day4/5_reverse.cpp
+++ cpp/day4/5_reverse.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-        int array[100], number, i, j, temporary;
+	srand (time(NULL));
+        int number, i, j;
+       	double temporary;
 
         cout << "Enter total number of elements: ";
         cin >> number;
-
-        cout << "Enter " << number << " number:\n";
-        for(i = 0; i < number; i++)
+	double array[number];
+        for (i = 0; i < number; i++)
 	{
-        	cin >> array[i];
+        	array[i] = ((rand() % 300) - 300) / 10;
 	}
 	cout << endl << "Original array: " << endl;
-	for(i = 0; i < number; i++)
+	for (i = 0; i < number; i++)
 	{
-        	cout << array[i]<<" ";
+        	cout << array[i] << "; ";
 	}
 	cout << endl << "Final array: " << endl;;
-	for(i = (number-1); i >= 0; i--)
+	for (i = (number - 1); i >= 0; i--)
 	{
-		cout << array[i]<<" ";
+		cout << array[i] << "; ";
 	}
         cout << endl;
 return 0;

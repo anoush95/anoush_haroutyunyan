f3fde80a16fc7ab25e57a567e57458207270f6ec
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-21 01:26:50 +0400

    Added the random function for the numbers of the array

--- cpp/day4/4_sort.cpp
+++ cpp/day4/4_sort.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-        int array[100], number, i, j, temporary;
+	srand (time(NULL));
+
+        int number, i, j;
+       	double temporary;
 
         cout << "Enter total number of elements: ";
         cin >> number;
-        cout << "Enter " << number << " number:\n";
-        for(i = 0; i < number; i++)
+	double array[number];
+        for (i = 0; i < number; i++)
 	{
-                cin >> array[i];
+                array[i] = ((rand() % 300 ) - 300) / 10.0;
 	}
 	cout << "Original array: ";
-        for(i=0; i < number; i++)
+        for (i = 0; i < number; i++)
 	{
-		cout << array[i]<<" ";
+		cout << array[i] <<"; ";
 
 	}	
-	for(i=0; i < number; i++)
+	for (i = 0; i < number; i++)
 	{
-		for(j = i + 1; j < number; j++)
+		for (j = i + 1; j < number; j++)
 		{
-			if(array[i] > array[j])
+			if (array[i] > array[j])
 			{
 				temporary = array[i];
 				array[i] = array[j];
@@ -31,10 +36,10 @@ int main()
 		}
 	}
 	cout << endl << "Final array: ";
-	for(i = 0; i < number; i++)
+	for (i = 0; i < number; i++)
 	{
 
-		cout << array[i] << ' ';
+		cout << array[i] << "; ";
 	}
 	cout << endl;
 return 0;

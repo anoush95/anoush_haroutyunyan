bc94ca1bdfe69fdca4bd01d453a1aec10fd250a8
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-21 00:22:19 +0400

    Made some corrections to the syntax

--- cpp/day4/3_largest_num.cpp
+++ cpp/day4/3_largest_num.cpp
@@ -6,27 +6,29 @@ int main()
         int array[100], number, i, largest;
         cout << "Enter total number of elements: ";
         cin >> number;
-        cout << "Enter " << number << " number:\n";
+        cout << "Enter " << number << " numbers:\n";
+        {
+                for(i = 0; i < number; i++)
+                {
+                        cin >> array[i];
 
-        for(i = 0; i < number; i++)
-                cin >> array[i];
-		cout << endl;
-	for(i = 0; i < number; i++)
-		cout << array[i ];
-		cout << endl;
+                }
+        }
+        cout << endl;
         largest = array[0];
-        for(i=0; i < number; i++)
+        for(i = 0; i < number; i++)
         {
                 if(largest < array[i])
                 {
                         largest = array[i];
                 }
-
         }
         cout << "The largest number is " << largest << endl;
 
 return 0;
 }
+
+
 
 
 

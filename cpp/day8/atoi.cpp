a99a46cd58945608b21af30a494810a585f4426c
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-21 00:19:04 +0400

    Made some corrections concerning negative numbers

--- cpp/day8/atoi.cpp
+++ cpp/day8/atoi.cpp
@@ -2,25 +2,26 @@
 #include "atoi.h"
 using namespace std;
 
-int myAtoi (char* string) 
+int myAtoi (char* string)
 {
-   int result = 0;
-   for (int i = 0; string[i] != '\0'; i++) 
-   {
-	if(string[i] >= '0' && string[i] <= '9')
-	{
-      		result = result * 10 + string[i] - '0';
-	
-		if (string[0] == '-')
-		
-		 	result = -result;
-		
-	}
-	
-   } 
-   return result;
+        int result = 0;
+        for (int i = 0; string[i] != '\0'; i++)
+        {
+                if(string[i] >= '0' && string[i] <= '9')
+                {
+                        result = result * 10 + string[i] - '0';
+                }
+                else
+                {
+                        result = 0;
+                }
+        }
+        if (string[0] == '-')
+        {
+                result -= result * 2;
+        } 
+return result;
 }
-      
 
 
 

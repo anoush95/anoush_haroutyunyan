eea714aaf69eacf5eefe7f068a6dae9c10a03216
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-22 22:10:15 +0400

    2nd task of day11

---
+++ cpp/day11/2_orthogonal_lines.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int x[4], y[4];
+	for(int i = 0; i < 4; i++)
+	{
+        	cin >> x[i] >> y[i];
+	}
+
+	if ((x[1] - x[0]) * (x[3] - x[2]) + (y[3] - y[2]) * (y[1] - y[0]) == 0)
+	{
+        	cout << "Yes\n";
+	}
+	else
+	{
+        	cout << "No\n";
+	}
+
+return 0;
+}
+

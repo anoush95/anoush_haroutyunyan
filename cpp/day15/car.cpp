2e41a6fac2737068cbc3e224cb7dd245f4f0262b
Anoush <anoush.haroutyunyan95@gmail.com>
2022-03-30 23:14:30 +0400

    Made some improvements

--- cpp/day15/car.cpp
+++ cpp/day15/car.cpp
@@ -6,9 +6,10 @@ using namespace std;
 struct Car
 {
 	string brand;
+	string model;
 	int price;
 	int mileage;
-	float engine;
+	int engine;
 	string gearbox;
 	string colour;
 
@@ -16,6 +17,8 @@ struct Car
         {	
 		cout << "===============================" << endl;
                 cout << "Car brand | " << brand << endl;
+		cout << "-------------------------------" << endl;
+		cout << "model     | " << model << endl;
 		cout << "-------------------------------" << endl;
                 cout << "price 	  | " << price << endl;
 		cout << "-------------------------------" << endl;
@@ -28,6 +31,7 @@ struct Car
                 cout << "colour    | " << colour << endl;
 		cout << "===============================" << endl;
         }
+
 };
 int main ()
 {
@@ -37,68 +41,72 @@ int main ()
 	string fcars;
 	int i = 0;
 	int line = 0;
-	string line_words[6];
+	string line_words[7];
 	while (ifcar >> fcars)
 	{
 		line_words[i] = fcars;
 		i++;
-		if (i == 6)
+		if (i == 7)
 		{
-			for (int j = 0; j < 5; j++)
+			for (int j = 0; j < 6; j++)
 			{
 				line_words[j].pop_back();
-			}
+			}	
 			car[line].brand = line_words[0];
-			car[line].price = atoi(line_words[1].c_str());
-			car[line].mileage = atoi(line_words[2].c_str());
-			car[line].engine = atof(line_words[3].c_str());
-			car[line].gearbox = line_words[4];
-			car[line].colour = line_words[5];
+			car[line].model = line_words[1];
+			car[line].price = atoi(line_words[2].c_str());
+			car[line].mileage = atoi(line_words[3].c_str());
+			car[line].engine = atoi(line_words[4].c_str());
+			car[line].gearbox = line_words[5];
+			car[line].colour = line_words[6];
 			i = 0;
 			line++;
 		}
 	}
 	ifcar.close ();
 	string filter, str_filter;
-	float float_filter;
-	int min, max;
-	cout << "Choose a filter (brand, price, mileage, engine, gearbox, colour): ";
-	cin >> filter;
-	if (filter == "brand" || filter == "gearbox" || filter == "colour")
-	{
-		cout << "Input " << filter << ": ";
-		cin >> str_filter;
+	int number, x = 0;
+	cout << "Choose the number of filters: ";
+	cin >>  number;
+	if ( number > 7 || number < 1)
+		{
+			cout << "Invalid input\n";
+			return 0;
+
+		}
+	cout << "Choose " << number << " filters (brand, model, price, mileage, engine, gearbox, colour): ";
+	cin.ignore();
+	getline (cin, filter);
+	cout << "Input " << filter << " filters ";
+        getline (cin, str_filter);	
+	string str [number];
+	for (int i = 0; str_filter[i] != '\0'; i++)
+        {
+                if (str_filter[i] == ' ')
+                {
+                        x++;
+                        continue;
+                }
+               str[x] += str_filter[i];
 	}
-	if (filter == "mileage" || filter == "engine")
-	{
-		cout << "Input " << filter << ": ";
-		cin >> float_filter;
-	}
-	if (filter == "price")
-	{
-		cout << "min price: ";
-		cin >> min;
-		cout << "max price: ";
-		cin >> max;
-	}
-	cout << endl;
+	int k = 0;
 	for (int i = 0; i < n; i++)
 	{
-		if (car[i].brand == str_filter || car[i].mileage == float_filter || car[i].engine == float_filter || car[i].gearbox == str_filter || car[i].colour == str_filter)
+		for(int j = 0; j < number; j++)
 		{
-			car[i].print();
-			
-		}
-		if (filter == "price")
-		{
-			if (car[i].price >= min && car[i].price <= max)
+			if (car[i].brand == str[j] || car[i].model == str[j] || car[i].price == atoi(str[j].c_str()) || car[i].mileage == atoi(str[j].c_str()) || car[i].engine == atof(str[j].c_str()) || car[i].gearbox == str[j] || car[i].colour == str[j])
 			{
-				car[i].print();
-				
+				k++;
 			}
-		}	
-	
-	}	
+		       	if( k == number)
+		       	{
+				car[i].print();
+			       
+		       	}
+		}
+		k = 0;
+	}
+		
 
 return 0;
 }
